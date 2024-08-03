9ddb79a16b14a313b1a71648cbf0713a3c07967e
Иванцов Александр <ivancov@kinoplan.tech>
2024-08-03 08:34:07 +0300

    all changes after review in Kinoplan

---
+++ SpaceX_Kinoplan/App/SomeShit.swift
@@ -0,0 +1,19 @@
+//
+//  SomeShit.swift
+//  SpaceX_Kinoplan
+//
+//  Created by Александр on 01.03.2024.
+//
+
+import UIKit
+
+protocol SomeShitProtocol: UIView {
+    
+}
+extension SomeShitProtocol {
+    func doSomething() {
+        print(1)
+    }
+}
+//let button = UIButton()
+//button.doSomething()
